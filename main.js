ab3d36ce683a45f9d27214c3c27f06e96cc8d180
Bharat Gupta <36244814+virat07@users.noreply.github.com>
2018-09-28 18:14:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 document.getElementById('mySearch').addEventListener('keyup', loadUsers);
 function loadUsers() {
     var api = 'https://rest.bandsintown.com/artists/';
-    var city = document.getElementById('mySearch').value;
+    var band = document.getElementById('mySearch').value;
     var key = '?app_id="bharat"';
-    var URL = api+city+key;
+    var URL = api+band+key;
     var xhr = new XMLHttpRequest();
     xhr.open('GET', URL, true);
     xhr.onload = function () {
         if (this.status == 200) {
             var users = JSON.parse(this.responseText);
             var output = ' ';
-            output += '<div class = "artist">' + '<img src="' + users.thumb_url + '">' +
+            output += '<div>' + '<img src="' + users.thumb_url + '">' +
                 '<ul>' +
                 '<li class="name"> ' + users.name +' ' +'<span >'+ '<a id="link" class = "fa fa-facebook" target="_blank" href='+users.facebook_page_url+'>'+'</a>'+'</span>'+'</li>' +
                 
@@ -18,25 +18,26 @@ function loadUsers() {
                 '</ul>' +
                 '</div>';
 
-            document.getElementById('artist').innerHTML = output;
+            document.getElementById('artists').innerHTML = output;
             
         }
+       
     }
     xhr.send();
     loadevents();
 }
 function loadevents(){
     var api = 'https://rest.bandsintown.com/artists/';
-    var city = document.getElementById('mySearch').value;   
+    var band = document.getElementById('mySearch').value;   
     var key2 = '/events?app_id="bharat"';
-    var URL_EVENTS = api + city + key2;
+    var URL_EVENTS = api + band + key2;
     var xhr2 = new XMLHttpRequest();
     xhr2.open('GET',URL_EVENTS,true);
     xhr2.onload = function(){
         var user = JSON.parse(this.responseText);
         var out = ' ';
         for(i in user){
-        out += '<div>'+' '+user[i].venue.country+','+user[i].venue.city+' '+user[i].venue.name+' ' + '</div>';
+        out += '<table>'+'<td>'+user[i].venue.city+'</td>'+'<td>'+user[i].venue.country+'</td>'+' '+'<td>'+user[i].venue.name+'</td>'+' '+ '</table>';
     
         }
     document.getElementById('upcomingevents').innerHTML = out;
